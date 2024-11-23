3a5734b8bc8a508542f8e68bbcc2afc6ab994e21
mukagen <tech0.mukainakano@gmail.com>
2024-11-23 11:00:00 +0900

    Delete pages directory

--- pages/api/hello.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-    if (req.method === 'GET') {
-      res.status(200).json({ message: 'Hello World' });
-    } else if (req.method === 'POST') {
-      const { input } = req.body;
-      res.status(200).json({ message: input });
-    }
-  }
-  
